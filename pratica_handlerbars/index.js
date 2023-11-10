7c5df6cd5e9cad3eb9e0c5ddc6053a4e09b54fac
AntoniaNeres <antonia.neres.oliveira07@aluno.ifce.edu.br>
2023-11-10 16:20:09 -0300

    pratica_handlerbars

---
+++ pratica_handlerbars/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const port = 3000
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+app.use(express.static('public'))
+
+//GET
+app.get('/users/add', (req, res) => {
+    res.render('userform')
+    })
+    
+    app.post('/users/save', (req, res) => {
+    const name = req.body.name
+    const age = req.body.age
+    
+    const user = { name: name, age: age }
+    res.render('viewuser', { user: user })
+    
+    })
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(' Server Started')
+  })
